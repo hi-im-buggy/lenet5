0898bf22b37f7ffc434c88a8582b760b3c50480e
Pratyaksh Gautam <pratyakshgautam11@gmail.com>
2022-12-28 01:50:51 +0530

    Add training seperation to driver code and model paths

--- config.py
+++ config.py
@@ -6,8 +6,12 @@ import wandb
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 BATCH_SIZE = 16
-NUM_EPOCHS = 10
+NUM_EPOCHS = 30
 LEARNING_RATE = 1e-3
+MODEL_SAVE_PATH = './models/'
+MODEL_LOAD_PATH = './models/model-2022-12-28-01:45:50-0'
+
+training = True
 
 wandb.config = {
     'batch_size': BATCH_SIZE,
--- main.py
+++ main.py
@@ -19,15 +19,20 @@ dataset = MNIST('./data', train=True, download=True, transform=t)
 train_set, val_set = random_split(dataset, [50000, 10000])
 test_set = MNIST('./data', train=False, download=True, transform=t)
 
-train_loader = DataLoader(train_set, batch_size=BATCH_SIZE)
-val_loader = DataLoader(val_set, batch_size=BATCH_SIZE)
-test_loader = DataLoader(test_set, batch_size=BATCH_SIZE)
+train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
+val_loader = DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=True)
 
 model = LeNet5()
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-train(model, train_loader, val_loader, optimizer)
-model.train(False)
+if training:
+    train(model, train_loader, val_loader, optimizer)
+
+else:
+    model.load_state_dict(torch.load(MODEL_LOAD_PATH))
+    model.eval()
+
 test(model, test_loader)
 
 wandb.watch(model)
